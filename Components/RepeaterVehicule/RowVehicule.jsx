fff887c6b43688844301d0885879c64010aa0d13
Zaszen <franck_hilcenko@hotmail.com>
2017-05-09 14:48:06 +0200

    correction

--- Components/RepeaterVehicule/RowVehicule.jsx
+++ Components/RepeaterVehicule/RowVehicule.jsx
@@ -1,22 +1,45 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router';
 
 require('./RowVehicule.css');
+
+var VoletDetails = require('./VoletDetails.jsx');
+
 class RowVehicules extends React.Component{
     constructor(props){
         super(props);
-    };
+        this.state = {
+            showVoletDetail: false
+        }
+    }
+    onClick(e){
+        if(this.state == true){
+            this.setState({showVoletDetail: false});
+        }else{
+            this.setState({showVoletDetail: true});
+        }
+    }
     render() {
+        let volet;
+
+        if (this.state.showVoletDetail) {
+            volet = ( <VoletDetails /> )
+        }else{
+            volet = ( <div></div>)
+        }
         return(
-            <div className = "rowVehicule" data-id={this.props.cle}>
-                <span className="identifiant"> {this.props.vehicule.id} </span> 
-                <span className="modele"> Modèle: {this.props.vehicule.modele} </span> 
-                <span className="etat"> Statut: {this.props.vehicule.etat} </span>
-                <span className="details" data-id={this.props.cle}></span>
+            <div>
+                <div className = "rowVehicule" data-id={this.props.cle}>
+                    <span className="identifiant"> {this.props.vehicule.id} </span> 
+                    <span className="modele"> Modèle: {this.props.vehicule.modele} </span> 
+                    <span className="etat"> Statut: {this.props.vehicule.etat} </span>
+                    <span className="details" data-id={this.props.cle}  onClick={this.onClick.bind(this)}></span>
+                </div>
+                { volet }  
             </div>
         );
     }
-}
+}     
 
 module.exports = RowVehicules;
---
+++ Components/RepeaterVehicule/VoletDetails.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react');
+
+class VoletDetails extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div className="volet-details"></div>
+        );
+    }
+}
+
+module.exports = VoletDetails;
