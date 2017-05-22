8f4ac6624f55abf4e7f268166d29785d807cf24b
Zaszen <franck_hilcenko@hotmail.com>
2017-05-22 22:53:56 +0200

    ajout input recherche + range selector

--- Components/RechercheAvance/RechercheAvance.jsx
+++ Components/RechercheAvance/RechercheAvance.jsx
@@ -1,11 +1,16 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var enhanceWithClickOutside = require('react-click-outside');
-require('./RechercheAvance.css')
+var InputRange = require('react-input-range');
+require('react-input-range/lib/css/index.css');
+require('./RechercheAvance.css');
 
 class RechercheAvance extends React.Component{
     constructor(props){
         super(props)
+        this.state = {
+            value: { min: 2, max: 10 },
+        };
     }
     handleClickOutside() {
         this.props.closeRecherche();
@@ -17,7 +22,34 @@ class RechercheAvance extends React.Component{
     render(){
         return(
                 <div id='recherche-avance'>
-                   
+                    <div>Recherche Avancée</div>
+                    <label htmlFor="recherche-modele">Modèle:</label>
+                    <select name="recherche-modele">
+                        <option value="volvo">Volvo</option>
+                        <option value="saab">Saab</option>
+                        <option value="fiat">Fiat</option>
+                        <option value="audi">Audi</option>
+                    </select>
+                    <label htmlFor="recherche-statut">Statut:</label>
+                    <select name="recherche-statut">
+                        <option value="loue">loué</option>
+                        <option value="toto">toto</option>
+                        <option value="rofl">Rofl</option>
+                        <option value="mop">Mop</option>
+                    </select>
+                    <label htmlFor="recherche-lieu">Lieu:</label>
+                    <select name="recherche-Lieu">
+                        <option value="foo">foo</option>
+                        <option value="toto">toto</option>
+                        <option value="rofl">Rofl</option>
+                        <option value="mop">Mop</option>
+                    </select>
+                    <InputRange
+                        maxValue={20}
+                        minValue={0}
+                        value={this.state.value}
+                        onChange={value => this.setState({ value })} 
+                    />
                 </div>
         )
     }
