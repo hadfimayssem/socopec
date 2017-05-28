97472bd61cfa76282554c738e82681e88d1161d6
Zaszen <franck_hilcenko@hotmail.com>
2017-05-28 10:44:44 +0200

    maj package.json

--- Components/RechercheAvanceAdmin/RechercheAvanceAdmin.jsx
+++ Components/RechercheAvanceAdmin/RechercheAvanceAdmin.jsx
@@ -21,35 +21,43 @@ class RechercheAvanceAdmin extends React.Component{
 
     render(){
         return(
-                <div id='recherche-avance'>
-                    <div>Recherche Avancée</div>
-                    <label htmlFor="recherche-modele">Modèle:</label>
-                    <select name="recherche-modele">
-                        <option value="volvo">Volvo</option>
-                        <option value="saab">Saab</option>
-                        <option value="fiat">Fiat</option>
-                        <option value="audi">Audi</option>
-                    </select>
-                    <label htmlFor="recherche-statut">Statut:</label>
-                    <select name="recherche-statut">
-                        <option value="loue">loué</option>
-                        <option value="toto">toto</option>
-                        <option value="rofl">Rofl</option>
-                        <option value="mop">Mop</option>
-                    </select>
-                    <label htmlFor="recherche-lieu">Lieu:</label>
-                    <select name="recherche-Lieu">
-                        <option value="foo">foo</option>
-                        <option value="toto">toto</option>
-                        <option value="rofl">Rofl</option>
-                        <option value="mop">Mop</option>
-                    </select>
-                    <InputRange
-                        maxValue={20}
-                        minValue={0}
-                        value={this.state.value}
-                        onChange={value => this.setState({ value })} 
-                    />
+                <div id='recherche-avance-admin'>
+                    <div id='wrapper-recherche-bt'>
+                        <div id='recherche-bt-vehicule'></div>
+                        <div id='recherche-bt-agent'></div>
+                        <div id='recherche-bt-agence'></div>
+                        <div id='recherche-bt-etat'></div>
+                    </div>
+                    <div id='recherche-avance-vehicule'>
+                        <div>Recherche Avancée</div>
+                        <label htmlFor="recherche-modele">Modèle:</label>
+                        <select name="recherche-modele">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            <option value="fiat">Fiat</option>
+                            <option value="audi">Audi</option>
+                        </select>
+                        <label htmlFor="recherche-statut">Statut:</label>
+                        <select name="recherche-statut">
+                            <option value="loue">loué</option>
+                            <option value="toto">toto</option>
+                            <option value="rofl">Rofl</option>
+                            <option value="mop">Mop</option>
+                        </select>
+                        <label htmlFor="recherche-lieu">Lieu:</label>
+                        <select name="recherche-Lieu">
+                            <option value="foo">foo</option>
+                            <option value="toto">toto</option>
+                            <option value="rofl">Rofl</option>
+                            <option value="mop">Mop</option>
+                        </select>
+                        <InputRange
+                            maxValue={20}
+                            minValue={0}
+                            value={this.state.value}
+                            onChange={value => this.setState({ value })} 
+                        />
+                    </div>
                 </div>
         )
     }
