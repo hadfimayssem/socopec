0f5b0e1db61c81faf70eadd49e81bb694c4904be
Zaszen <franck_hilcenko@hotmail.com>
2017-05-28 11:45:11 +0200

    ajout choix recherche avance admin

--- Components/RechercheAvanceAdmin/RechercheAvanceAdmin.jsx
+++ Components/RechercheAvanceAdmin/RechercheAvanceAdmin.jsx
@@ -10,23 +10,86 @@ class RechercheAvanceAdmin extends React.Component{
         super(props)
         this.state = {
             value: { min: 2, max: 10 },
+            vehicule: true,
+            agent: false,
+            agence: false,
+            etat: false
         };
+        this.clickChangeActiveVehicule = this.clickChangeActiveVehicule.bind(this);
+        this.clickChangeActiveAgent = this.clickChangeActiveAgent.bind(this);
+        this.clickChangeActiveAgence = this.clickChangeActiveAgence.bind(this);
+        this.clickChangeActiveEtat = this.clickChangeActiveEtat.bind(this);
     }
     handleClickOutside() {
         this.props.closeRecherche();
+    }
+    clickChangeActiveVehicule(){
+        this.setState({
+            vehicule: true,
+            agent: false,
+            agence: false,
+            etat: false
+        })
+    }
+    clickChangeActiveAgent(){
+        this.setState({
+            vehicule: false,
+            agent: true,
+            agence: false,
+            etat: false
+        })
+    }
+    clickChangeActiveAgence(){
+        this.setState({
+            vehicule: false,
+            agent: false,
+            agence: true,
+            etat: false
+        })
+    }
+    clickChangeActiveEtat(){
+        this.setState({
+            vehicule: false,
+            agent: false,
+            agence: false,
+            etat: true
+        })
     }
     // validerRecherche(){
     //     this.props.closePopup();
     // }
 
     render(){
+        var activeVehicule, activeEtat, activeAgent,activeAgence;
+        if(this.state.vehicule == true){
+            activeVehicule = "active";
+            activeAgent = "";
+            activeAgence = "";
+            activeEtat = "";
+        }else if(this.state.agent == true){
+            activeVehicule = "";
+            activeAgent = "active";
+            activeAgence = "";
+            activeEtat = "";
+        }else if(this.state.agence == true){
+            activeVehicule = "";
+            activeAgent = "";
+            activeAgence = "active";
+            activeEtat = "";
+        }else if(this.state.etat == true){
+            activeVehicule = "";
+            activeAgent = "";
+            activeAgence = "";
+            activeEtat = "active";
+        }
+
         return(
                 <div id='recherche-avance-admin'>
                     <div id='wrapper-recherche-bt'>
-                        <div id='recherche-bt-vehicule'></div>
-                        <div id='recherche-bt-agent'></div>
-                        <div id='recherche-bt-agence'></div>
-                        <div id='recherche-bt-etat'></div>
+                        <div id='recherche-bt-vehicule' className={activeVehicule} onClick={this.clickChangeActiveVehicule.bind(this)}></div>
+                        <div id='recherche-bt-agent' className={activeAgent} onClick={this.clickChangeActiveAgent.bind(this)}></div>
+                        <div id='recherche-bt-agence' className={activeAgence} onClick={this.clickChangeActiveAgence.bind(this)}></div>
+                        <div id='recherche-bt-etat' className={activeEtat} onClick={this.clickChangeActiveEtat.bind(this)}></div>
                     </div>
                     <div id='recherche-avance-vehicule'>
                         <div>Recherche Avancée</div>
